acac309e04fcc9ec3ab1d41c66cd92e855a5c02c
John Powell <john_powell@partech.com>
2023-02-03 16:27:15 -0500

    Updated to support headless multi SITL

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 
 function show_help {
     echo ""
-    echo "Usage: ${0} [-h | -v VEHICLE | -w WORLD] [IP_API | IP_QGC IP_API]"
+    echo "Usage: ${0} [-h | -v VEHICLE | -w WORLD | -c COUNT | -a IP_API | -q IP_QGC]"
     echo ""
     echo "Run a headless px4-gazebo simulation in a docker container. The"
     echo "available vehicles and worlds are the ones available in PX4"
     echo "(i.e. when running e.g. \`make px4_sitl gazebo_iris__baylands\`)"
     echo ""
+    echo "  -a    Set the API IP (default: localhost)"
+    echo "  -c    Set the instance count (default: 1)"
     echo "  -h    Show this help"
+    echo "  -q    Set the QGroundControl IP (default: localhost)"
     echo "  -v    Set the vehicle (default: iris)"
     echo "  -w    Set the world (default: empty)"
     echo ""
@@ -22,12 +25,19 @@ OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
 vehicle=iris
 world=empty
+count=1
 
-while getopts "h?v:w:" opt; do
+while getopts "a:c:h?q:v:w:" opt; do
     case "$opt" in
+    a)  IP_API=$OPTARG
+        ;;
+    c)  count=$OPTARG
+        ;;
     h|\?)
         show_help
         exit 0
+        ;;
+    q)  IP_QGC=$OPTARG
         ;;
     v)  vehicle=$OPTARG
         ;;
@@ -38,9 +48,8 @@ done
 
 shift $((OPTIND-1))
 
-# All the leftover arguments are supposed to be IPs
-for arg in "$@"
-do
+# Checkout IP addresses if they have been set
+for arg in $IP_QGC $IP_API 10.0.0.1; do
     if ! [[ ${arg} =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         echo "Error: invalid IP: ${arg}!"
         echo ""
@@ -49,12 +58,21 @@ do
     fi
 done
 
-if [ "$#" -eq 1 ]; then
-    IP_QGC="$1"
-elif [ "$#" -eq 2 ]; then
-    IP_API="$1"
-    IP_QGC="$2"
-elif [ "$#" -gt 2 ]; then
+# validate the vehicle count
+if [ "${count}" -lt 1 -o "${count}" -gt 255 ]; then
+    echo "Error: invalid vehicle count: ${count}!"
+    echo ""
+    show_help
+    exit 1
+fi
+
+
+# check for 'secret' backdoor to the bash shell
+if [ "$#" -gt 0 ]; then
+    if [ "$1" = "bash" -o "$1" = "/bin/bash" ]; then
+        exec /bin/bash
+    fi
+
     show_help
     exit 1;
 fi
@@ -62,6 +80,26 @@ fi
 Xvfb :99 -screen 0 1600x1200x24+32 &
 ${SITL_RTSP_PROXY}/build/sitl_rtsp_proxy &
 
-source ${WORKSPACE_DIR}/edit_rcS.bash ${IP_API} ${IP_QGC} &&
-cd ${FIRMWARE_DIR} &&
-HEADLESS=1 make px4_sitl gazebo_${vehicle}__${world}
+if [ "x${IP_API}" = "x" ]; then
+  if [ "x${IP_QGC}" = "x" ]; then
+    source ${WORKSPACE_DIR}/edit_rcS.bash
+  else
+    source ${WORKSPACE_DIR}/edit_rcS.bash -q ${IP_QGC}
+  fi
+elif [ "x${IP_QGC}" = "x" ]; then
+  source ${WORKSPACE_DIR}/edit_rcS.bash -a ${IP_API} 
+else
+  source ${WORKSPACE_DIR}/edit_rcS.bash -a ${IP_API} -q ${IP_QGC}
+fi
+
+if [ $? -eq 0 ]; then
+    if [ "${count}" -gt 1 ]; then
+        cd ${FIRMWARE_DIR} &&
+        HEADLESS=1 Tools/gazebo_sitl_multiple_run.sh -n $count -m $vehicle -w $world
+    else
+        cd ${FIRMWARE_DIR} &&
+        HEADLESS=1 make px4_sitl gazebo_${vehicle}__${world}
+    fi
+else
+    exit 1
+fi
