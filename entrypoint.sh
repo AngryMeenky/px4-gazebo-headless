cf60f43f67b0818d5c9f4e7a74906530730085e0
John Powell <john_powell@partech.com>
2023-06-27 16:40:18 -0400

    Added HITL mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,13 @@ function show_help {
     echo "(i.e. when running e.g. \`make px4_sitl gazebo_iris__baylands\`)"
     echo ""
     echo "  -a    Set the API IP (default: localhost)"
+    echo "  -b    Set the PX4 baud rate (default: 921600)"
     echo "  -c    Set the instance count (default: 1)"
     echo "  -h    Show this help"
+		echo "  -H    Perform hardware in the loop simulation"
     echo "  -q    Set the QGroundControl IP (default: localhost)"
-    echo "  -v    Set the vehicle (default: iris)"
+		echo "  -s    Set the serial port root (default: /dev/ttyACM)"
+    echo "  -v    Set the vehicle (default: iris_hitl)"
     echo "  -w    Set the world (default: empty)"
     echo ""
     echo "  <IP_API> is the IP to which PX4 will send MAVLink on UDP port 14540"
@@ -23,13 +26,19 @@ function show_help {
 
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
-vehicle=iris
+serial=/dev/ttyACM
+baud=921600
+
+vehicle=iris_hitl
 world=empty
 count=1
+DO_HITL=0
 
-while getopts "a:c:h?q:v:w:" opt; do
+while getopts "a:b:c:h?Hq:s:v:w:" opt; do
     case "$opt" in
     a)  IP_API=$OPTARG
+        ;;
+    b)  baud=$OPTARG
         ;;
     c)  count=$OPTARG
         ;;
@@ -37,8 +46,12 @@ while getopts "a:c:h?q:v:w:" opt; do
         show_help
         exit 0
         ;;
+		H)  DO_HITL=1
+			  ;;
     q)  IP_QGC=$OPTARG
         ;;
+		s)  serial=$OPTARG
+			  ;;
     v)  vehicle=$OPTARG
         ;;
     w)  world=$OPTARG
@@ -77,8 +90,10 @@ if [ "$#" -gt 0 ]; then
     exit 1;
 fi
 
-Xvfb :99 -screen 0 1600x1200x24+32 &
-${SITL_RTSP_PROXY}/build/sitl_rtsp_proxy &
+if [ "${count}" -eq 1 ]; then
+    Xvfb :99 -screen 0 1600x1200x24+32 &
+    ${SITL_RTSP_PROXY}/build/sitl_rtsp_proxy &
+fi
 
 if [ "x${IP_API}" = "x" ]; then
   if [ "x${IP_QGC}" = "x" ]; then
@@ -93,9 +108,14 @@ else
 fi
 
 if [ $? -eq 0 ]; then
-    if [ "${count}" -gt 1 ]; then
-        cd ${FIRMWARE_DIR} &&
-        HEADLESS=1 Tools/gazebo_sitl_multiple_run.sh -n $count -m $vehicle -w $world
+    if [ "${count}" -gt 1 -o "${DO_HITL}" -eq 1 ]; then
+			  if [ "${DO_HITL}" -eq 1 ]; then
+            cd ${FIRMWARE_DIR} &&
+            HEADLESS=1 Tools/gazebo_hitl_multiple_run.sh -n $count -m $vehicle -w $world -b $baud -r $serial
+				else
+            cd ${FIRMWARE_DIR} &&
+            HEADLESS=1 Tools/gazebo_sitl_multiple_run.sh -n $count -m $vehicle -w $world
+				fi
     else
         cd ${FIRMWARE_DIR} &&
         HEADLESS=1 make px4_sitl gazebo_${vehicle}__${world}
